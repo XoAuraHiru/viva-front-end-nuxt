c43a93c22b0c7e49a5c017689dad9c1074d2bb8d
Hirun Chamara <astronomyhirunchamara@gmail.com>
2024-01-11 21:56:19 +0530

    Add: Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   app: {
     head: {
       "meta": [
@@ -31,5 +32,10 @@ export default defineNuxtConfig({
       ]
     }
 
+  },
+
+  modules: ["@pinia/nuxt"],
+  alias: {
+    pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
   },
 })
