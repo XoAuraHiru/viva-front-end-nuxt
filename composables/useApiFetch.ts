9c3f78372fbd8a830a3137c460f9bd9df5873127
Hirun Chamara <astronomyhirunchamara@gmail.com>
2024-01-13 03:00:34 +0530

    Fix: Add  cors

--- composables/useApiFetch.ts
+++ composables/useApiFetch.ts
@@ -1,10 +1,16 @@
-import type {UseFetchOptions} from 'nuxt/app'
-import {useRequestHeaders} from "nuxt/app";
+import type { UseFetchOptions } from 'nuxt/app'
+import { useRequestHeaders } from "nuxt/app";
 
 export function useApiFetch<T>(path: string, options: UseFetchOptions<T> = {}) {
   let headers: any = {
     accept: "application/json",
-    referer: "http://vivafront.xoaurahiru.com"
+    referer: "http://vivafront.xoaurahiru.com",
+    'Access-Control-Allow-Origin': 'Same-Origin',
+    'crossOriginResourcePolicy': 'same-origin',
+    'crossOriginOpenerPolicy': 'same-origin',
+    'crossOriginEmbedderPolicy': 'require-corp',
+    'contentSecurityPolicy': "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
+    'X-XSS-Protection': 1
   }
 
   const token = useCookie('XSRF-TOKEN');
