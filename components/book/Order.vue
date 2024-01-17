652e665be41d747ef0b379c2a117c63a9599c31b
Hirun Chamara <astronomyhirunchamara@gmail.com>
2024-01-17 22:48:20 +0530

    Fix: payhere sdk

--- components/book/Order.vue
+++ components/book/Order.vue
@@ -9,38 +9,37 @@ const props = defineProps({
 
 const order = props.orderInfo[0];
 
-console.log(order);
+const makePayment = () => {
+    if (window.PayHere) {
+        const payment = {
+            "sandbox": true,
+            "merchant_id": "NDA1Mjk5NzQ0MTE2MDM0NjI3OTAyMjUxNzYxMjMwNTI0NTIyNzI=",    // Replace your Merchant ID
+            "return_url": undefined,     // Important
+            "cancel_url": undefined,     // Important
+            "notify_url": "http://sample.com/notify",
+            "order_id": order.order_id,
+            "items": "Movie Tickets",
+            "amount": order.amount,
+            "currency": "LKR",
+            "hash": "45D3CBA93E9F2189BD630ADFE19AA6DC", // *Replace with generated hash retrieved from backend
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email,
+            "phone": "0771234567",
+            "address": "No.1, Galle Road",
+            "city": "Colombo",
+            "country": "Sri Lanka",
+            "delivery_address": "No. 46, Galle road, Kalutara South",
+            "delivery_city": "Kalutara",
+            "delivery_country": "Sri Lanka",
+            "custom_1": "",
+            "custom_2": ""
+        };
 
-const payment = {
-        "sandbox": true,
-        "merchant_id": "NDA1Mjk5NzQ0MTE2MDM0NjI3OTAyMjUxNzYxMjMwNTI0NTIyNzI=",    // Replace your Merchant ID
-        "return_url": undefined,     // Important
-        "cancel_url": undefined,     // Important
-        "notify_url": "http://sample.com/notify",
-        "order_id": order.order_id,
-        "items": "Movie Tickets",
-        "amount": order.amount,
-        "currency": "LKR",
-        "hash": "45D3CBA93E9F2189BD630ADFE19AA6DC", // *Replace with generated hash retrieved from backend
-        "first_name": user.first_name,
-        "last_name": user.last_name,
-        "email": user.email,
-        "phone": "0771234567",
-        "address": "No.1, Galle Road",
-        "city": "Colombo",
-        "country": "Sri Lanka",
-        "delivery_address": "No. 46, Galle road, Kalutara South",
-        "delivery_city": "Kalutara",
-        "delivery_country": "Sri Lanka",
-        "custom_1": "",
-        "custom_2": ""
-    };
+        window.PayHere.startPayment(payment);
+    }
+};
 
-onMounted(() => {
-    document.getElementById('payhere-payment').onclick = function (e) {
-        payhere.startPayment(payment);
-    };
-})
 </script>
 
 <template>
@@ -55,7 +54,7 @@ onMounted(() => {
 
             <span>{{ order }}</span>
 
-            <GeneralButtonFill class="mt-5 card__top" type="submit" id="payhere-payment">Pay Now</GeneralButtonFill>
+            <GeneralButtonFill @click="makePayment()" class="mt-5 card__top" type="button" id="payhere-payment">Pay Now</GeneralButtonFill>
 
 
         </div>
