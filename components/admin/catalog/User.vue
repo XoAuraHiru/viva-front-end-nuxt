82d870849986db5bd2224d059ff8d36815a2ef72
Hirun Chamara <astronomyhirunchamara@gmail.com>
2024-01-20 07:50:09 +0530

    Fix: order fetch not working

--- components/admin/catalog/User.vue
+++ components/admin/catalog/User.vue
@@ -1,11 +1,24 @@
+<script setup>
+    const props = defineProps({
+        name: {
+            type: String,
+            default: ''
+        },
+        email: {
+            type: String,
+            default: ''
+        }
+    })
+</script>
+
 <template>
     <div class="catalog__user">
         <div class="catalog__avatar">
             <img src="/img/user.svg" alt="">
         </div>
         <div class="catalog__meta">
-            <h3>Tess Harper</h3>
-            <span>email@email.com</span>
+            <h3>{{ props.name }}</h3>
+            <span>{{ props.email }}</span>
         </div>
     </div>
 </template>
--- pages/admin/orders.vue
+++ pages/admin/orders.vue
@@ -3,68 +3,58 @@ import { useAdmin } from "~/stores/useAdmin";
 definePageMeta({
     layout: 'admin'
 })
-
 const admin = useAdmin()
-
 const orders = ref([])
-
-async function getOrders() {
-    const { data } = await admin.getOrders()
-    orders.value = data
-}
-
-onMounted(() => {
-    getOrders()
-})
+const { data } = await admin.getOrders()
+orders.value = data
 
 </script>
 
 <template>
     <!-- main content -->
-	<main class="main">
-		<div class="container-fluid">
-			<div class="row">
-				<!-- main title -->
-				<div class="col-12">
-					<div class="main__title">
-						<h2>Users</h2>
+    <main class="main">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- main title -->
+                <div class="col-12">
+                    <div class="main__title">
+                        <h2>Users</h2>
 
-						<span class="main__title-stat">3,702 Total</span>
+                        <span class="main__title-stat">3,702 Total</span>
 
-						<div class="main__title-wrap">
-							<select class="filter__select" name="sort" id="filter__sort">
-								<option value="0">Date created</option>
-								<option value="1">Pricing plan</option>
-								<option value="2">Status</option>
-							</select>
+                        <div class="main__title-wrap">
+                            <select class="filter__select" name="sort" id="filter__sort">
+                                <option value="0">Date created</option>
+                                <option value="1">Pricing plan</option>
+                                <option value="2">Status</option>
+                            </select>
 
-							<!-- search -->
-							<form action="#" class="main__title-form">
-								<input type="text" placeholder="Find user..">
-								<button type="button">
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z"></path></svg>
-								</button>
-							</form>
-							<!-- end search -->
-						</div>
-					</div>
-				</div>
-				<!-- end main title -->
+                            <!-- search -->
+                            <form action="#" class="main__title-form">
+                                <input type="text" placeholder="Find user..">
+                                <button type="button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <path
+                                            d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z">
+                                        </path>
+                                    </svg>
+                                </button>
+                            </form>
+                            <!-- end search -->
+                        </div>
+                    </div>
+                </div>
+                <!-- end main title -->
 
-				<AdminOrdersTable :orders="orders" />
+                <AdminOrdersTable :orders="orders" />
 
-				<!-- paginator -->
+                <!-- paginator -->
 
-				<AdminPagination />
-			</div>
-		</div>
-	</main>
-	<!-- end main content -->
-
-    
-
+                <AdminPagination />
+            </div>
+        </div>
+    </main>
+    <!-- end main content -->
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
